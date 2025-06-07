60fc8a909bc19f9ab7895a1fdb0ee95afafcb6c6
Keshav Kannan <127866387+JemappleK@users.noreply.github.com>
2025-06-07 20:51:11 +0100

    yay

--- keshav.py
+++ keshav.py
@@ -44,6 +44,8 @@ class Rover():
         Initialize the rover's parameters
         wheels => list of wheels (1st 3 on the left, the other 3 on the right side of the rover)
         '''
+        self.r1 = 1
+        self.r2 = 1
         self.wheels = wheels
 
         self.max_turn_angle = max_turn_angle
@@ -63,13 +65,13 @@ class Rover():
         if  self.angle > def_angle:
             # if turning to the left
             self.wheels[0].angle = self.angle
-            r1 = get_radius(self.wheels[0].angle)  # Inner radius
+            self.r1 = get_radius(self.wheels[0].angle)  # Inner radius
     
             # Calculate the outer radius (for outer front wheel)
-            r2 = get_radius(self.wheels[0].angle) + WIDTH / 2  # Outer radius
+            self.r2 = get_radius(self.wheels[0].angle) + WIDTH / 2  # Outer radius
             
             # Calculate the outer wheel angle (for outer front wheel)
-            self.wheels[3].angle = np.rad2deg(np.arctan(LENGTH / r2))  # Outer right wheel angle
+            self.wheels[3].angle = np.rad2deg(np.arctan(LENGTH / self.r2))  # Outer right wheel angle
             
             # Rear wheels (steering in opposite direction for sharper turn)
             self.wheels[2].angle = -self.wheels[0].angle  # Rear left wheel
@@ -77,13 +79,13 @@ class Rover():
         elif self.angle < def_angle:
             # if turning to the right
             self.wheels[3].angle = self.angle
-            r1 = get_radius(self.wheels[3].angle)  # Inner radius
+            self.r1 = get_radius(self.wheels[3].angle)  # Inner radius
     
             # Calculate the outer radius (for outer front wheel)
-            r2 = get_radius(self.wheels[3].angle) - WIDTH / 2  # Outer radius
+            self.r2 = get_radius(self.wheels[3].angle) - WIDTH / 2  # Outer radius
             
             # Calculate the outer wheel angle (for outer front wheel)
-            self.wheels[0].angle = np.rad2deg(np.arctan(LENGTH / r2))  # Outer right wheel angle
+            self.wheels[0].angle = np.rad2deg(np.arctan(LENGTH / self.r2))  # Outer right wheel angle
             
             # Rear wheels (steering in opposite direction for sharper turn)
             self.wheels[2].angle = -self.wheels[0].angle  # Rear left wheel
@@ -97,43 +99,46 @@ PORT = 8765
 serial = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 async def connect(websocket):
-    print("[*] Connection Established!")
-    
-    # Listen for commands
-    while True:
-        try:
-            outer_speed = 255
-            inner_speed = outer_speed/Rover.r2*Rover.r1
-            # Wait for a command
-            command = await websocket.recv(4)
-            print(f"[*] Recieved: {command}")
+	rover = Rover(wheels=wheels, max_turn_angle=180)
+	print("[*] Connection Established!")
+	# Listen for commands
+	while True:
+		try:
+			outer_speed = 255
+			inner_speed = outer_speed/rover.r2*rover.r1
+			# Wait for a command
+			command = await websocket.recv(4)
+			print(f"[*] Recieved: {command}")
 
-            Rover.turn(1)
+			# Rover.turn(1)
 
-            # Do something about the recieved command her
-            
-            if command[1] == "1":
-                serial.write(b'\xff\xff\xff\xff\xff\xff')
-            elif command[2] == "1":
-                serial.write(b'\x00\x00\x00\x00\x00\x00')
-            else:
-                serial.write(b"\x7f\x7f\x7f\x7f\x7f\x7f")
-            if command[3] == "1":
-                right = bytearray([outer_speed, outer_speed, outer_speed, inner_speed, inner_speed, inner_speed])
-                serial.write(right)
-            if command[4] == "1":
-                left = bytearray([inner_speed, inner_speed, inner_speed, outer_speed, outer_speed, outer_speed])
-                serial.write(left)
+			# Do something about the recieved command her
 
-        except websockets.exceptions.ConnectionClosed as e:
-            print(f"[*] Connection Closed: {e}")
-            break
+			if command[1] == "1":
+				pass
+				#serial.write(b'\xff\xff\xff\xff\xff\xff')
+			elif command[2] == "1":
+				pass
+				#serial.write(b'\x00\x00\x00\x00\x00\x00')
+			else:
+				pass
+				#serial.write(b"\x7f\x7f\x7f\x7f\x7f\x7f")
+			if command[3] == "1":
+				right = bytearray([outer_speed, outer_speed, outer_speed, inner_speed, inner_speed, inner_speed])
+				#serial.write(right)
+			if command[0] == "1":
+				left = bytearray([inner_speed, inner_speed, inner_speed, outer_speed, outer_speed, outer_speed])
+				#serial.write(left)
+
+		except websockets.exceptions.ConnectionClosed as e:
+			print(f"[*] Connection Closed: {e}")
+			break
 
 
 async def main():
-    async with websockets.serve(connect, "0.0.0.0", PORT):
-        print(f"[*] Server Started on Port {PORT}")
-        await asyncio.Future() # run forever
+	async with websockets.serve(connect, "0.0.0.0", PORT):
+		print(f"[*] Server Started on Port {PORT}")
+		await asyncio.Future() # run forever
 
 
 # Start the Server
